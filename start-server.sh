439d2f6e7c4b1ff16d490112197e3453ae1b3e74
Rafael Walter <rafawalter@gmail.com>
2016-03-15 21:15:24 -0300

    controle do servidor com forever

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,2 @@
-node ./node_modules/http-server/bin/http-server &
+# node ./node_modules/http-server/bin/http-server &
+./node_modules/forever/bin/forever start ./node_modules/http-server/bin/http-server
--- stop-server.sh
+++ stop-server.sh
@@ -1,1 +1,2 @@
-kill $(ps aux | grep '[n]ode ./node_modules/http-server/bin/http-server' | awk '{print $2}')
+# kill $(ps aux | grep '[n]ode ./node_modules/http-server/bin/http-server' | awk '{print $2}')
+./node_modules/forever/bin/forever stop ./node_modules/http-server/bin/http-server
